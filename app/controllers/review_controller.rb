ecae45ad65f30a97a221d1b60d907828ee86e2b2
Antonia Lappin <antonia.lappin@gmx.de>
2022-05-18 16:21:35 +0200

    super

---
+++ app/controllers/review_controller.rb
@@ -0,0 +1,16 @@
+class ReviewController < ApplicationController
+  def show
+    @review = Review.find(params[:id])
+  end
+
+  def new
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(review_params)
+    @review.save
+
+    redirect_to restaurants_path(@restaurants)
+  end
+end
---
+++ app/models/review.rb
@@ -0,0 +1,6 @@
+class Review < ApplicationRecord
+  validates :rating, inclusion: 0..5, numericality: { only_integer: true }
+  validates :content, presence: true
+
+  belongs_to :restaurant
+end
